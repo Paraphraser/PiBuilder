6d6cab6e02ca89d0676e337f59eda0df25d4eb16
Phill Kelley <pmk.57t49@lgosys.com>
2021-05-09 14:47:24 +1000

    Make boot volume setup script useful on non-Mac platforms
    Senses if running on macOS and:
    
    1. Presents messages as notifications (as before)
    2. Expands `eject__VolumeWithPrejudice` (a previously un-supplied
    script) to be inline. In general, the "crud" macOS puts on volumes to
    support Spotlight metadata does no harm to non-Mac systems but I prefer
    to be tidy and remove it during the "eject" process.
    
    If not running on macOS:
    
    1. Presents messages via `echo`, which should work on Linux.
    2. Bypasses Mac-specific cleanup. No attempt is made to eject the
    volume on non-Mac systems. A placeholder is left as an exercise for
    the reader.

--- setup_boot_volume.sh
+++ setup_boot_volume.sh
@@ -4,33 +4,77 @@
 WHERE=$(realpath "$0")
 WHERE=$(dirname "$WHERE")
 
+# sense running on macOS
+[ "$(uname -s)" == "Darwin" ] && isMacOS=true
+
 # the volume name is
 BOOTVOLUME="boot"
 
-# define the source and target
+# define the source directory
 BOOTSOURCE="$WHERE/$BOOTVOLUME"
+
+# this is where the boot volume mounts on macOS
 BOOTTARGET="/Volumes/$BOOTVOLUME"
+
+# a general-purpose reporting function
+report () {
+   if [ $isMacOS ] ; then
+      osascript -e "display notification \"$1\" with title \"RPi setup\""
+   else
+      echo "RPi setup: $1"
+   fi
+}
 
 # does the boot source exist?
 if [ ! -d "$BOOTSOURCE" ] ; then
-   osascript -e 'display notification "boot directory not found." with title "RPi setup"'
+   report "boot directory not found."
    exit 0
 fi
 
 # does the target exist?
 if [ ! -d "$BOOTTARGET" ] ; then
-   osascript -e 'display notification "boot volume not mounted." with title "RPi setup"'
+   report "boot volume not mounted."
    exit 0
 fi
-
-# make sure the boot source doesn't have any DSStores
-find "$BOOTSOURCE" -name .DS_Store -delete
 
 # copy the **contents** of the boot directory to the boot target
 cp -aX "$BOOTSOURCE"/* "$BOOTTARGET"
 
-# now eject it
-eject__VolumeWithPrejudice $BOOTVOLUME
+# running macOS ?
+if [ $isMacOS ] ; then
+
+   # yes make sure we are not in this volume (it's about to be ejected)
+   cd
+
+   # tell spotlight to ignore the volume
+   touch "$BOOTTARGET/.metadata_never_index"
+
+   # turn spotlight off for this volume (it might be on already)
+   mdutil -i off "$BOOTTARGET"
+
+   # erase any spotlight metadata stores that might have been built
+   mdutil -E "$BOOTTARGET"
+
+   # kill any .DS_Store files
+   find "$BOOTTARGET" -name ".DS_Store" -delete
+
+   # next, kill any resource-fork shadows
+   find "$BOOTTARGET" -name "._*" -delete
+
+   # remove other macOS-specific stuff
+   rm -rf \
+      "$BOOTTARGET/.Trashes/" \
+      "$BOOTTARGET/.fseventsd/" \
+      "$BOOTTARGET.Spotlight-V100/"
+
+   # eject the volume
+   /usr/sbin/diskutil eject "$BOOTTARGET"
+
+else
+
+   report "If you have OS-specific cleanup and ejection routines, put them here"
+
+fi
 
 # declare complete
-osascript -e 'display notification "boot volume set up." with title "RPi setup"'
+report "boot volume set up - safe to remove."
