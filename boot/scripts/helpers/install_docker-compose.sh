3b360e3ddca5ecfe8592fd1d2dc10fc87a092ebd
Phill Kelley <34226495+Paraphraser@users.noreply.github.com>
2022-08-29 16:49:16 +1000

    bump default docker-compose version to 2.10.2
    Signed-off-by: Phill Kelley <34226495+Paraphraser@users.noreply.github.com>

--- boot/scripts/helpers/install_docker-compose.sh
+++ boot/scripts/helpers/install_docker-compose.sh
@@ -50,7 +50,7 @@
 SCRIPT=$(basename "$0")
 
 # the default version of docker-compose at the moment is
-DOCKER_COMPOSE_VERSION_DEFAULT="v2.10.1"
+DOCKER_COMPOSE_VERSION_DEFAULT="v2.10.2"
 
 read -r -d '' COMPOSENOTES <<-EOM
 \n
