d7973e5aef1b14319da0c0ca273e47d9cb1d7c46
Phill Kelley <34226495+Paraphraser@users.noreply.github.com>
2024-02-19 13:46:27 +1100

    bump default version of docker-compose installed via script to v2.24.6
    Signed-off-by: Phill Kelley <34226495+Paraphraser@users.noreply.github.com>

--- boot/scripts/helpers/install_docker-compose.sh
+++ boot/scripts/helpers/install_docker-compose.sh
@@ -74,7 +74,7 @@ if is_running_OS_release bookworm ; then
 fi
 
 # the default version of docker-compose at the moment is
-DOCKER_COMPOSE_VERSION_DEFAULT="v2.24.5"
+DOCKER_COMPOSE_VERSION_DEFAULT="v2.24.6"
 
 read -r -d '' COMPOSENOTES <<-EOM
 \n
